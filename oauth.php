7b3dc744af57afc0a289235e57b6cd1a6b54a0cd
gonejack <igonejack@gmail.com>
2020-04-11 11:26:53 +0800

    Update: improvements and fixes.
    Signed-off-by: gonejack <igonejack@gmail.com>

--- oauth.php
+++ oauth.php
@@ -8,24 +8,25 @@
 
 require 'vendor/autoload.php';
 
+use Everimg\App\Conf;
+
 $sandbox = false;
-$china   = false;
+$china = false;
 
 $oauth_handler = new \Evernote\Auth\OauthHandler($sandbox, false, $china);
 
-$key      = Conf::get('consumer.key');
-$secret   = Conf::get('consumer.secret');
+$key = Conf::get('consumer.key');
+$secret = Conf::get('consumer.secret');
 $callback = 'http://localhost:1234/oauth.php';
 
 try {
-    $oauth_data  = $oauth_handler->authorize($key, $secret, $callback);
+    $oauth_data = $oauth_handler->authorize($key, $secret, $callback);
 
     echo "\nOauth Token : " . $oauth_data['oauth_token'];
 
     // Now you can use this token to call the api
     $client = new \Evernote\Client($oauth_data['oauth_token']);
-}
-catch (Evernote\Exception\AuthorizationDeniedException $e) {
+} catch (Evernote\Exception\AuthorizationDeniedException $e) {
     //If the user decline the authorization, an exception is thrown.
     echo "Declined";
 }
--- src/App/Log.php
+++ src/App/Log.php
@@ -13,6 +13,9 @@ namespace Everimg\App;
 use Everimg\Lib\Logger;
 
 class Log {
+    private static $stdout;
+    private static $stderr;
+    private static $container = [];
     private static $allows = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'];
 
     public static function init(): bool {
@@ -20,54 +23,6 @@ class Log {
 
         static::$allows = array_slice(static::$allows, array_search($threshold, static::$allows));
 
-        return LoggerManager::init();
-    }
-
-    public static function debug(string $msg, ...$args) {
-        if (static::checkLevel('DEBUG')) {
-            LoggerManager::get(self::getModuleName())->debug($msg, ...$args);
-        }
-    }
-
-    public static function info(string $msg, ...$args) {
-        if (static::checkLevel('INFO')) {
-            LoggerManager::get(self::getModuleName())->info($msg, ...$args);
-        }
-    }
-
-    public static function warn(string $msg, ...$args) {
-        if (static::checkLevel('ERROR')) {
-            LoggerManager::get(self::getModuleName())->warn($msg, ...$args);
-        }
-    }
-
-    public static function error(string $msg, ...$args) {
-        if (static::checkLevel('ERROR')) {
-            LoggerManager::get(self::getModuleName())->error($msg, ...$args);
-        }
-    }
-
-    public static function fatal(string $msg, ...$args) {
-        if (static::checkLevel('FATAL')) {
-            LoggerManager::get(self::getModuleName())->fatal($msg, ...$args);
-        }
-    }
-
-    private static function checkLevel(string $level): bool {
-        return in_array($level, static::$allows);
-    }
-
-    private static function getModuleName(): string {
-        return debug_backtrace()[2]['class'];
-    }
-}
-
-class LoggerManager {
-    private static $container = [];
-    private static $stdout;
-    private static $stderr;
-
-    public static function init(): bool {
         if (Conf::getEnv("LOG_TO_FILE", false)) {
             static::redirectStream();
         } else {
@@ -75,20 +30,49 @@ class LoggerManager {
             self::$stderr = STDERR;
         }
 
-        static::$container = [];
-
         return true;
     }
 
-    public static function get($name): Logger {
+    public static function debug(string $msg, ...$args) {
+        if (static::checkLevel('DEBUG')) {
+            static::getLogger(self::getModuleName())->debug($msg, ...$args);
+        }
+    }
+    public static function info(string $msg, ...$args) {
+        if (static::checkLevel('INFO')) {
+            static::getLogger(self::getModuleName())->info($msg, ...$args);
+        }
+    }
+    public static function warn(string $msg, ...$args) {
+        if (static::checkLevel('ERROR')) {
+            static::getLogger(self::getModuleName())->warn($msg, ...$args);
+        }
+    }
+    public static function error(string $msg, ...$args) {
+        if (static::checkLevel('ERROR')) {
+            static::getLogger(self::getModuleName())->error($msg, ...$args);
+        }
+    }
+    public static function fatal(string $msg, ...$args) {
+        if (static::checkLevel('FATAL')) {
+            static::getLogger(self::getModuleName())->fatal($msg, ...$args);
+        }
+    }
+
+    private static function redirectStream() {
+        // 不需要重定向至文件
+    }
+    private static function checkLevel(string $level): bool {
+        return in_array($level, static::$allows);
+    }
+    private static function getLogger($name): Logger {
         if (!isset(self::$container[$name])) {
             self::$container[$name] = new Logger($name, self::$stdout, self::$stderr);
         }
 
         return self::$container[$name];
     }
-
-    private static function redirectStream() {
-        // 不需要重定向至文件
+    private static function getModuleName(): string {
+        return debug_backtrace()[2]['class'];
     }
 }
